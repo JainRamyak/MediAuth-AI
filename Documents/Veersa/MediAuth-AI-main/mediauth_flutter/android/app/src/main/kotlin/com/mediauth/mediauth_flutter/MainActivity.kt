5bdb53b2720cd8a4961144cbb9ec98fcc7cd9aff
Ramyak Jain <jain.ramyak0987@gmail.com>
2026-05-05 06:57:10 +0530

    Delete Documents/Veersa/MediAuth-AI-main/mediauth_flutter directory

--- Documents/Veersa/MediAuth-AI-main/mediauth_flutter/android/app/src/main/kotlin/com/mediauth/mediauth_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.mediauth.mediauth_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Documents/Veersa/MediAuth-AI-main/mediauth_flutter/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mediauth_flutter/main.dart';
-
-void main() {
-  testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MediAuthApp());
-    expect(find.byType(MediAuthApp), findsOneWidget);
-  });
-}
